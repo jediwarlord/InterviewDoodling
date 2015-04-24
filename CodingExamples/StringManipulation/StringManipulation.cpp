b55d9068de50c180b79bd14948ae34b03179aa5a
jediwarlord <jedi.warlord@gmail.com>
2015-04-23 22:02:01 -0700

    string stuff

---
+++ CodingExamples/StringManipulation/StringManipulation.cpp
@@ -0,0 +1,22 @@
+// StringManipulation.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	if (argc < 2)
+	{
+		printf("Please enter a string for reversing...\n");
+		return 0;
+	}
+
+	// string was entered and we want to reverse it now. 
+	if (argv[0] == "-r")
+	{
+
+	}
+	return 0;
+}
+
